8a7a8929cf16304c6c1d9df82f7f37a1d3858c13
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-08-02 16:49:27 -0500

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -2,3 +2,29 @@
 
 # home Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install dependencies
+sudo apt-get install -y libpcap-dev build-essential
+
+# Download and install masscan
+MASSCAN_VERSION="1.0.6"  # Update this to the latest version if needed
+wget https://github.com/robertdavidgraham/masscan/archive/refs/tags/${MASSCAN_VERSION}.tar.gz
+tar -xzf ${MASSCAN_VERSION}.tar.gz
+cd masscan-${MASSCAN_VERSION}
+
+# Compile and install masscan
+make
+sudo make install
+
+# Clean up
+cd ..
+rm -rf masscan-${MASSCAN_VERSION} ${MASSCAN_VERSION}.tar.gz
+
+echo "Masscan and dependencies have been installed."
